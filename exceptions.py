abbac6c5ca1f160bccba03fdf0f0a1652646749c
lovernj <71782977+lovernj@users.noreply.github.com>
2022-06-16 20:45:00 -0700

    Update exceptions.py

--- exceptions.py
+++ exceptions.py
@@ -1,7 +1,9 @@
-
+"""
+All the exceptions used in this program. 
+"""
 class LoginException(Exception):
     """Credentials were not accepted in some step."""
 class StateException(Exception):
     """An invalid state was detected."""
 class EarlyLeaveException(Exception):
-    """Leave the run process early."""
+    """Leave the run process early. Controlled panic, basically."""
